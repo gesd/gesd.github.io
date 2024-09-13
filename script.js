59482a4c1ea65ed84adc245de11af0d434740138
gesd <80385857+gesd@users.noreply.github.com>
2024-09-13 10:06:42 +1000

    Transfer

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav a').forEach((link) => {
+    link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const target = document.querySelector(e.target.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
